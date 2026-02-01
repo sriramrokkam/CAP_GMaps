7122a4b83b76cbf879087b2c31d552c394d17e03
Sriram.Rokkam <s.rokkam@sap.com>
2026-02-01 06:42:18 +0530

    added authorizations

---
+++ app/routes/dist/utils/Config.js
@@ -0,0 +1,2 @@
+sap.ui.define([],function(){"use strict";return{getApiKey:function(){return window.GOOGLE_MAPS_API_KEY||"AIzaSyBnJ6XNmu3vQE6Uay9BX7q1HV-Qz_N5eP4"}}});
+//# sourceMappingURL=Config.js.map
