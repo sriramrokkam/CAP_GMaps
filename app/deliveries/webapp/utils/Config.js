ce4261d867e86b0d2cca7de5b57afb5c570eb196
Sriram.Rokkam <s.rokkam@sap.com>
2026-04-16 09:02:04 +0530

    feat: add Config.js API key helper for deliveries app

---
+++ app/deliveries/webapp/utils/Config.js
@@ -0,0 +1,10 @@
+// app/deliveries/webapp/utils/Config.js
+sap.ui.define([], function () {
+    "use strict";
+    return {
+        getApiKey: function () {
+            return window.GOOGLE_MAPS_API_KEY ||
+                   "AIzaSyBnJ6XNmu3vQE6Uay9BX7q1HV-Qz_N5eP4";
+        }
+    };
+});
